4e62c26e9498d00ac3b7a3fa91d66ca3773cfb58
Giannis Koutsoukos <iokouts@gmail.com>
2021-09-23 17:44:23 +0300

    fixed order of null values

--- app/controllers/api/v1/airports_controller.rb
+++ app/controllers/api/v1/airports_controller.rb
@@ -4,14 +4,11 @@ class API::V1::AirportsController < API::V1::BaseController
   after_action { pagy_headers_merge(@pagy) if @pagy }
   
   def index
-    @airports = Airport.all.order(passenger_volume: :desc)
+    @airports = Airport.all.order('passenger_volume DESC NULLS LAST')
 
     if params[:countries].present?
       @airports = @airports.where(country_alpha2: params[:countries])
     end
-
-    # params[:per_page] = 5
-    # params[:page] = 6
 
     @pagy, @records = pagy(@airports, items: params[:per_page], page: params[:page])
 
